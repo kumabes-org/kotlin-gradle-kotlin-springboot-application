aa31979c8fa24a40328ba78d45347d72b0c5966a
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2023-03-05 17:25:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# kotlin-gradle-kotlin-springboot-application
+```
+git update-index --chmod=+x gradlew gradlew.bat
+```
---
+++ app/src/main/kotlin/br/com/kumabe/KotlinGradleKotlinSpringbootApplication.kt
@@ -0,0 +1,11 @@
+package br.com.kumabe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinGradleKotlinSpringbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinGradleKotlinSpringbootApplication>(*args)
+}
